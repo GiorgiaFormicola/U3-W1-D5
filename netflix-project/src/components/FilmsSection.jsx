802e7227f8f52ddc77a785a28f878f0736097605
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-02-06 15:42:25 +0100

    Loaders and errors handle done

--- netflix-project/src/components/FilmsSection.jsx
+++ netflix-project/src/components/FilmsSection.jsx
@@ -1,12 +1,17 @@
 import { Component } from "react";
 import Row from "react-bootstrap/Row";
+import Alert from "react-bootstrap/Alert";
+import Placeholder from "react-bootstrap/Placeholder";
 import SingleFilm from "./SingleFilm";
+import SingleFilmPlaceholder from "./SingleFilmPlaceholder";
 // const key = "14895418";
 
 class FilmsSection extends Component {
   state = {
     searchURL: `http://www.omdbapi.com/?apikey=14895418&s=${this.props.searchString}`,
     searchArray: [],
+    loading: true,
+    error: false,
   };
 
   getFilms = () => {
@@ -23,10 +28,16 @@ class FilmsSection extends Component {
         this.setState({
           ...this.state,
           searchArray: searchObject.Search,
+          loading: false,
         });
       })
       .catch((error) => {
         console.log("ERROR", error);
+        this.setState({
+          ...this.state,
+          loading: false,
+          error: true,
+        });
       });
   };
 
@@ -37,12 +48,41 @@ class FilmsSection extends Component {
   render() {
     return (
       <>
-        <h4>The {this.props.searchString} Saga</h4>
-        <Row className="row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-6 mb-4">
-          {this.state.searchArray.slice(0, 6).map((film) => {
-            return <SingleFilm key={film.imdbID} filmSrc={film.Poster} filmTitle={film.Title}></SingleFilm>;
-          })}
-        </Row>
+        {this.state.loading && (
+          <>
+            <Placeholder as="h4" animation="glow">
+              <Placeholder xs={6} lg={4} className="rounded-pill" />
+            </Placeholder>
+            <Row className="row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-6 mb-4">
+              <SingleFilmPlaceholder></SingleFilmPlaceholder>
+              <SingleFilmPlaceholder></SingleFilmPlaceholder>
+              <SingleFilmPlaceholder></SingleFilmPlaceholder>
+              <SingleFilmPlaceholder></SingleFilmPlaceholder>
+              <SingleFilmPlaceholder></SingleFilmPlaceholder>
+              <SingleFilmPlaceholder></SingleFilmPlaceholder>
+            </Row>
+          </>
+        )}
+
+        {this.state.error && !this.state.loading && (
+          <>
+            <h4>Error</h4>
+            <Alert variant="black" className="text-light text-center py-5 my-5 bg-dark bg-opacity-50">
+              <p className="py-5 mb-0 fs-6">Ops! An error occurred while loading the data!</p>
+            </Alert>
+          </>
+        )}
+
+        {!this.state.error && !this.state.loading && (
+          <>
+            <h4>The {this.props.searchString} Saga</h4>
+            <Row className="row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-6 mb-4">
+              {this.state.searchArray.slice(0, 6).map((film) => {
+                return <SingleFilm key={film.imdbID} filmSrc={film.Poster} filmTitle={film.Title}></SingleFilm>;
+              })}
+            </Row>
+          </>
+        )}
       </>
     );
   }
