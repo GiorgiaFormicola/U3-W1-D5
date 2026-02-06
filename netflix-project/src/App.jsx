2236c3d70b6ea7bbd732da44296101ad59160728
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-02-06 13:15:05 +0100

    Genres navbar done

--- netflix-project/src/App.jsx
+++ netflix-project/src/App.jsx
@@ -2,11 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
 import "./App.css";
 import MyNavbar from "./components/MyNavbar";
+import MyHomepage from "./components/MyHomepage";
 import MyFooter from "./components/MyFooter";
+
 function App() {
   return (
     <>
       <MyNavbar></MyNavbar>
+      <MyHomepage></MyHomepage>
       <MyFooter></MyFooter>
     </>
   );
