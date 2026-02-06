7e5cafb5249f166cabcd74b4a643c559d5fe6197
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-02-06 12:07:38 +0100

    Footer component done

--- netflix-project/src/App.jsx
+++ netflix-project/src/App.jsx
@@ -2,10 +2,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
 import "./App.css";
 import MyNavbar from "./components/MyNavbar";
+import MyFooter from "./components/MyFooter";
 function App() {
   return (
     <>
       <MyNavbar></MyNavbar>
+      <MyFooter></MyFooter>
     </>
   );
 }
