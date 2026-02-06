4866dec110092ebd9e1f14fc8b5e9eda05febd71
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-02-06 14:15:46 +0100

    Single film structure updated

--- netflix-project/src/components/SingleFilm.jsx
+++ netflix-project/src/components/SingleFilm.jsx
@@ -4,8 +4,8 @@ import Col from "react-bootstrap/Col";
 class SingleFilm extends Component {
   render() {
     return (
-      <Col className="text-center mb-2 px-1">
-        <img className="img-fluid" src={this.props.filmSrc} alt={this.props.filmTitle} />
+      <Col className="d-flex justify-content-center mb-2 px-1">
+        <img className="img-fluid" src={this.props.filmSrc} alt={this.props.filmTitle} style={{ width: "18em" }} />
       </Col>
     );
   }
