f8228736007d207ca7fd4b9ce0a990c7862781b4
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-02-06 11:38:31 +0100

    Navbar component done

--- netflix-project/src/App.jsx
+++ netflix-project/src/App.jsx
@@ -1,6 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import "./App.css";
+import MyNavbar from "./components/MyNavbar";
 function App() {
-  return <></>;
+  return (
+    <>
+      <MyNavbar></MyNavbar>
+    </>
+  );
 }
 
 export default App;
